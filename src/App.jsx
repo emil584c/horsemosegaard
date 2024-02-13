a2dfc26bac99586ad78e412e164b1ded98a75404
emil584c <105641442+emil584c@users.noreply.github.com>
2024-02-13 01:02:12 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./styles/app.scss";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/HomePage";
+import MyWork from "./components/MyWork";
+import PortfolioItem from "./components/PortfolioItem";
+import portfolioItems from "./js/portfolioData";
+
+function App() {
+  return <></>;
+  // <Router>
+  //   <Routes>
+  //     <Route
+  //       path="/"
+  //       element={<Home />}
+  //     />
+  //     <Route
+  //       path="/portfolio"
+  //       element={<MyWork portfolioItems={portfolioItems} />}
+  //     />
+  //     <Route
+  //       path="/portfolio/:id"
+  //       element={<PortfolioItem portfolioItems={portfolioItems} />}
+  //     />
+  //   </Routes>
+  // </Router>
+}
+
+export default App;
