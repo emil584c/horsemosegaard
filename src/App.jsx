43c75695120a2ad1d0ae6cecce528d2ee5689245
emil584c <105641442+emil584c@users.noreply.github.com>
2024-02-13 01:15:24 +0100

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,18 @@ import "./styles/app.scss";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/HomePage";
-import MyWork from "./components/MyWork";
-import PortfolioItem from "./components/PortfolioItem";
-import portfolioItems from "./js/portfolioData";
 
 function App() {
-  return <></>;
-  // <Router>
-  //   <Routes>
-  //     <Route
-  //       path="/"
-  //       element={<Home />}
-  //     />
-  //     <Route
-  //       path="/portfolio"
-  //       element={<MyWork portfolioItems={portfolioItems} />}
-  //     />
-  //     <Route
-  //       path="/portfolio/:id"
-  //       element={<PortfolioItem portfolioItems={portfolioItems} />}
-  //     />
-  //   </Routes>
-  // </Router>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import PrimaryCover from "./PrimaryCover.jsx";
+
+function HomePage() {
+  return (
+    <>
+      <PrimaryCover />
+    </>
+  );
+}
+
+export default HomePage;
