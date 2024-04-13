78f3afb8c7188c56c171c42437e10fc28ef8838c
emil584c <105641442+emil584c@users.noreply.github.com>
2024-04-13 15:30:39 +0200

    price section

--- src/components/pages/Prices.jsx
+++ src/components/pages/Prices.jsx
@@ -1,6 +1,7 @@
 import HeaderSection from "../HeaderSection";
 import SecondaryCover from "../SecondaryCover";
 import FooterSection from "../FooterSection.jsx";
+import PriceSection from "../PriceSection";
 import ContactSection from "../ContactSection.jsx";
 
 export default function Prices() {
@@ -8,6 +9,31 @@ export default function Prices() {
     <>
       <HeaderSection />
       <SecondaryCover title="Priser" />
+      <PriceSection
+        title="Udlejning af lokaler"
+        text="Brødtekst til priser. vores forskellige priseleje, er der ikke det helt rigtige til dig? Så tag kontakt og så tager vi en snak."
+        prices={[
+          {
+            title: "Hverdag",
+            price: "3000 kr",
+            deposit: "500 kr",
+            text: "En enkelt dag/aften i en af hverdagene fra mandag-torsdag.",
+            listItems: [
+              "El-forbrug måles før og efter",
+              "Depositum indbetales ved bookning",
+              "Depositum tilbage betales ved annullering 90 dage før",
+              "Restbeløb indbetales 30 dage før",
+            ],
+          },
+          {
+            title: "Weekend",
+            price: "7000 kr",
+            deposit: "3000 kr",
+            text: "Book lokalet i en hel weekend fra fredag til og med søndag",
+            listItems: ["Liste punkt 1", "Liste punkt 2"],
+          },
+        ]}
+      />
       <ContactSection
         title="Kontakt os"
         text="Har i spørgsmål til vores faciliteter, eller har i lyst til at tage en snak om vi er det rette sted for jer, så send en besked over messenger osv osv."
